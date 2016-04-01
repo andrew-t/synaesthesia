e5d7fb62ee92eb5c480d20d876a0dfd5565c007f
Andrew Taylor <Andrew.Taylor04@bbc.co.uk>
2016-04-01 17:49:04 +0100

    Detect and colourise hex

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import commander from 'commander';
 import { colourise, emojise } from './visualise';
 
@@ -9,22 +11,29 @@ commander
 	//.option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', '//marble')
 	.parse(process.argv);
 
-visualise = colourise;
+var visualise = colourise;
 
 process.stdin.setEncoding('utf8');
 
 var data = '';
 process.stdin.on('readable', () => {
-	data += process.stdin.read();
-	var i;
-	while (i = data.indexOf() >= 0) {
-		process.stdout.write(visualise(data.substr(0, i)) + '\n');
-		data = data.substr(i + 1);
+	var chunk = process.stdin.read();
+	if (chunk) {
+		data += chunk;
+		var i;
+		while ((i = data.indexOf('\n')) >= 0) {
+			processLine(data.substr(0, i));
+			process.stdout.write('\n');
+			data = data.substr(i + 1);
+		}
 	}
 });
 
 process.stdin.on('end', () => {
-  // Do nothing
+	processLine(data);
 });
 
-
+function processLine(line) {
+	process.stdout.write(
+		line.replace(/[\da-f]{4,}/gi, visualise));
+}
